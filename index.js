2a539e8c67de404cb0f07f720a0bd95a227204e7
James Linnegar <jlinnega@thoughtworks.com>
2015-07-04 10:03:29 +0100

    moving head and footer out into partials

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var Metalsmith = require('metalsmith'),
+    Handlebars = require('handlebars'),
     markdown   = require('metalsmith-markdown'),
-    templates  = require('metalsmith-templates');
+    templates  = require('metalsmith-templates'),
+    fs         = require('fs');
+
+Handlebars.registerPartial('head', fs.readFileSync(__dirname + '/templates/partials/head.hbt').toString());
+Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/partials/footer.hbt').toString());
 
 Metalsmith(__dirname)
    .use(markdown())
