9b38b68092700c4d41b1982c36e028dd6cd54a31
James Linnegar <jlinnega@thoughtworks.com>
2015-07-08 17:16:02 +0100

    adding in navigation to pages

--- index.js
+++ index.js
@@ -3,27 +3,17 @@ var Metalsmith = require('metalsmith'),
     markdown   = require('metalsmith-markdown'),
     templates  = require('metalsmith-templates'),
     sass	   = require('metalsmith-sass'),
-    watch      = require('metalsmith-watch'),
-    serve      = require('metalsmith-serve'),
-    Sitemap    = require('metalsmith-sitemap')
+    sitemap    = require('metalsmith-sitemap')
     permalinks = require('metalsmith-permalinks'),
     collections = require('metalsmith-collections'),
     fs         = require('fs');
 
 
 Handlebars.registerPartial('head', fs.readFileSync(__dirname + '/templates/partials/head.hbt').toString());
+Handlebars.registerPartial('navigation', fs.readFileSync(__dirname + '/templates/partials/navigation.hbt').toString());
 Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/partials/footer.hbt').toString());
 
 Metalsmith(__dirname)
-   .use(serve({
-   	verbose:true
-   }))
-   .use(watch({
-   		paths: {
-        "**/*": true,
-        "templates/**/*": "**/*.md",
-      },
-      livereload: true}))
    .use(sass({
    	  outputStyle: "expanded",
    	  outputDir: 'css/'
@@ -34,7 +24,7 @@ Metalsmith(__dirname)
    	relative: false
    }))
    .destination('./build')
-   .use(Sitemap({
+   .use(sitemap({
    		output: 'sitemap.xml', // The location where the final sitemap should be placed
     	hostname: 'http://www.mudwetandbeers.com', // hostname to use for URL, if needed
     	modifiedProperty: 'modified', // Key for last modified property
